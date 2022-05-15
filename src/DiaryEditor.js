f830778aa0a7faad1224af5d87491dc0e101403e
Dohui Ko <bluecard722@hufs.ac.kr>
2022-05-15 19:32:13 +0900

    Add: create and delete function

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,9 +1,6 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const DiaryEditor = () => {
-
-  const authorInput = useRef();
-  const contentInput = useRef();
 
   const [state, setState] = useState({
     author: "",
@@ -22,16 +19,8 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    if(state.author.length < 1){
-      authorInput.current.focus();
-      return;
-    }
-    if(state.content.length <5 ){
-      contentInput.current.focus();
-      return;
-    }
-
-    alert('저장 성공!');
+    console.log(state);
+    alert('저장 성공');
   }
 
   return (
@@ -39,7 +28,6 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
-          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -47,7 +35,6 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
-          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
