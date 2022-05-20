f168b0c8df5f955be1fb3362d1725a9a6d841e87
Dohui Ko <bluecard722@hufs.ac.kr>
2022-05-20 10:51:07 +0900

    Add: React에서 사용자 입력 처리하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <DiaryEditor/>
+    <div className="App">
+      <DiaryEditor />
     </div>
   );
-}
-
+};
 export default App;
