86b67fb301634afa4ea6dee7e8cf1f60f58001ed
DohuiKo <bluecard722@hufs.ac.kr>
2022-06-01 14:09:20 +0900

    Add: React에서 배열 사용하기 1 - 리스트 렌더링 (조회)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from './DiaryList';
+import './App.css';
+
+const dummyList = [
+  {
+    id:1,
+    author:"고도희",
+    content:"하이 1",
+    emotion:1,
+    created_date: new Date().getTime() // 생성자에 아무것도 넣지 않고..
+  },
+  {
+    id:2,
+    author:"아무개",
+    content:"하이 1",
+    emotion:1,
+    created_date: new Date().getTime() // 생성자에 아무것도 넣지 않고..
+  },
+  {
+    id:3,
+    author:"아무러니",
+    content:"하이 1",
+    emotion:1,
+    created_date: new Date().getTime() // 생성자에 아무것도 넣지 않고..
+  },
+  {
+    id:4,
+    author:"호호",
+    content:"하이 1",
+    emotion:1,
+    created_date: new Date().getTime() // 생성자에 아무것도 넣지 않고..
+  },
+]
 
 const App = () => {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList = {dummyList}/>
     </div>
   );
 };
