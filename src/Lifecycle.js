79f8d9a45a2148090483b489e6ca288bb3170e52
DohuiKo <bluecard722@hufs.ac.kr>
2022-06-03 18:24:19 +0900

    Add: React Lifecycle제어하기 - useEffect

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,41 +1,25 @@
 import React, { useEffect, useState} from 'react';
 
-const Lifecycle = () => {
-  
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
+const UnmountTest = () => {
 
   useEffect(() => {
     console.log("Mount!");
-  }, []);
 
-  useEffect(()=>{
-    console.log("Update!");
-  });
-
-  useEffect(() => {
-    console.log(`count is update: ${count}`);
-
-    if(count > 5) {
-      alert('count가 5를 넘었습니다. 따라서 1로 초기화합니다');
-      setCount(1);
+    return()=> {
+      console.log("unMount!");
     }
+  }, []);
+  return <div>Unmount Testing Component</div>
+}
 
-  }, [count])
-
-  useEffect(() => {
-    console.log(`text is update: ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return(
-    <div style={{padding:20}}>
     <div>
-      {count}
-      <button onClick={()=>setCount(count+1)}>+</button>
-    </div>
-    <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-    </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && < UnmountTest />}
     </div>
   );
 };
